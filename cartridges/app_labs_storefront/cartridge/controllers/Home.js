7bb3f63a26cfdf69f417dd47ff5255d708628673
rustem.astound <r.mustafaiev@astoundcommerce.com>
2019-12-31 13:39:28 +0200

    NB-11720:LAB-8:ADD Generate CSFR token

--- cartridges/app_labs_storefront/cartridge/controllers/Home.js
+++ cartridges/app_labs_storefront/cartridge/controllers/Home.js
@@ -1,9 +1,9 @@
 'use strict';
 
 var server = require('server');    //the server module is used by all controllers
-var cache = require('*/cartridge/scripts/middleware/cache');
+var csrf = require('*/cartridge/scripts/middleware/csrf');
 
-server.get('Show', cache.applyDefaultCache, server.middleware.https, function (req, res, next) {  //registers the Show route for the Home module
+server.get('Show', csrf.generateToken, function (req, res, next) {  //registers the Show route for the Home module
     res.render('/home/homePage');      //renders the hompage template
     next();            //notifies middleware chain that it can move to the next step or terminate if this is the last step.
 });
